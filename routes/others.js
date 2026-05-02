c3a753b5a280318dc647a0c8317ba2dac197c7b0
ziyosen <ziyosenstore@gmail.com>
2026-05-03 04:25:57 +0700

    Add show-related endpoints to others.js

--- routes/others.js
+++ routes/others.js
@@ -4,12 +4,15 @@ import { WELCOME_BANNER } from "../utils/log.js";
 
 const otherRouter = Router();
 
+// Endpoint utama untuk Banner
 otherRouter.get("/", (req, res) => {
     res.send(`<pre>${WELCOME_BANNER}</pre>`);
 });
 
+// Pastikan semua fungsi ini ada di controllers/shows.js
+otherRouter.get("/shows", controller.handleGetAllShows);
+otherRouter.get("/show/:id", controller.handleGetShowById);
 otherRouter.get("/episode/:id", controller.handleGetEpisodeById);
-
 otherRouter.get("/search", controller.handleSearch);
 
 export default otherRouter;
